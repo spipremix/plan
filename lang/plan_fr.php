707933f7fb387c28ca0989d61db1e3889d2d24d6
salvatore <salvatore@rezo.net>
2015-11-01 04:33:02 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ plan] Export depuis http://trad.spip.net

--- lang/plan_fr.php
+++ lang/plan_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/plan/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
@@ -9,24 +9,23 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'confirmer_deplacement' => 'Êtes-vous sûr de vouloir déplacer ces éléments ?',
 
 	// D
+	'deplacement_en_echec' => 'Échec d’un déplacement.',
 	'deplacement_reussi' => 'Déplacement réussi.',
-	'deplacements_reussis' => '@nb@ déplacements réussis.',
-	'deplacement_en_echec' => "Échec d'un déplacement.",
 	'deplacements_en_echec' => '@nb@ déplacements en échec.',
-	
+	'deplacements_reussis' => '@nb@ déplacements réussis.',
 
 	// E
-	'erreur_deplacement_impossible' => "Déplacement impossible !",
-	'erreur_autorisation_insuffisante' => "Autorisation insuffisante.",
-	'erreur_aucun_identifiant' => "Aucun identifiant d'objet.",
-	'erreur_rubriques_parentes_incorrectes' => "Rubriques parentes incorrectes.",
-	'erreur_deplacement' => "Déplacement non effectué.",
+	'erreur_aucun_identifiant' => 'Aucun identifiant d’objet.',
+	'erreur_autorisation_insuffisante' => 'Autorisation insuffisante.',
+	'erreur_deplacement' => 'Déplacement non effectué.',
+	'erreur_deplacement_impossible' => 'Déplacement impossible !',
+	'erreur_rubriques_parentes_incorrectes' => 'Rubriques parentes incorrectes.',
 
 	// P
 	'plan_titre' => 'Plan du site dans l’espace privé',
 
 	// T
-	'tous_les_statuts' => 'Tous les statuts',
-
-
+	'tous_les_statuts' => 'Tous les statuts'
 );
+
+?>
