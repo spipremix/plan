6376569db7a1b6099f8bb7f84fc8832aafa682f1
jacques <jacques@jack31.net>
2016-02-07 04:34:58 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ plan] Export depuis http://trad.spip.net de la langue en
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/plan@94990 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/plan_en.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/plan?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_deplacement' => 'Are you sure you want to move all these elements?',
+
+	// D
+	'deplacement_en_echec' => 'Move failed.',
+	'deplacement_reussi' => 'Successful move.',
+	'deplacements_en_echec' => '@nb@ moves failed.',
+	'deplacements_reussis' => '@nb@ moves successful.',
+
+	// E
+	'erreur_aucun_identifiant' => 'No object identifier.',
+	'erreur_autorisation_insuffisante' => 'Insufficient permission.',
+	'erreur_deplacement' => 'Move not made.',
+	'erreur_deplacement_impossible' => 'Impossible move!',
+	'erreur_rubriques_parentes_incorrectes' => 'Incorrect parent sections.',
+
+	// P
+	'plan_titre' => 'Site Map in the private space.',
+
+	// T
+	'tous_les_statuts' => 'Status All'
+);
+
+?>
