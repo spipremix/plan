ee21ef581c651422a150b262dd971e8091612196
spip <spip@allesfrans.com>
2016-02-18 04:36:43 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ plan] Export depuis http://trad.spip.net de la langue nl

---
+++ lang/plan_nl.php
@@ -0,0 +1,35 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/plan?lang_cible=nl
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_deplacement' => 'Weet je zeker dat je deze elementen wilt veerplaatsen?',
+
+	// D
+	'deplacement_en_echec' => 'Verplaatsen is mislukt.',
+	'deplacement_reussi' => 'Verplaatsen voltooid.',
+	'deplacements_en_echec' => '@nb@ verplaatsingen mislukt.',
+	'deplacements_reussis' => '@nb@ verplaatsingen voltooid.',
+
+	// E
+	'erreur_aucun_identifiant' => 'Geen object-identificatie.',
+	'erreur_autorisation_insuffisante' => 'Onvoldoende rechten.',
+	'erreur_deplacement' => 'Verplaatsing niet uitgevoerd.',
+	'erreur_deplacement_impossible' => 'Verplaatsing onmogelijk!',
+	'erreur_rubriques_parentes_incorrectes' => 'Onjuiste bovenliggende rubrieken.',
+
+	// P
+	'plan_titre' => 'Sitemap in het privé gedeelte',
+
+	// T
+	'tous_les_statuts' => 'Alle statussen'
+);
+
+?>
