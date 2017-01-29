47bf1a9480508b70220e8810cb0739a5c228460a
george <george@middleeastwatch.net>
2017-01-29 04:33:12 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ plan] Export depuis http://trad.spip.net de la langue ar
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/plan@102546 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/plan_ar.php
@@ -0,0 +1,33 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/plan?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_deplacement' => 'تأكيد نقل هذه العناصر؟',
+
+	// D
+	'deplacement_en_echec' => 'فشل النقل.',
+	'deplacement_reussi' => 'نحج النقل.',
+	'deplacements_en_echec' => '@nb@ نقل فاشل.',
+	'deplacements_reussis' => '@nb@  نقل ناجح.',
+
+	// E
+	'erreur_aucun_identifiant' => 'لا يوجد اي معرف عنصر.',
+	'erreur_autorisation_insuffisante' => 'إجازة غير كافية.',
+	'erreur_deplacement' => 'لم يتم النقل.',
+	'erreur_deplacement_impossible' => 'النقل مستحيل!',
+	'erreur_rubriques_parentes_incorrectes' => 'الأقسام المحتوية غير صحيحة.',
+
+	// P
+	'plan_titre' => 'خريطة الموقع في المجال الخاص',
+
+	// T
+	'tous_les_statuts' => 'كل الوضعيات'
+);
