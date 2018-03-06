200ab0a33fd88e640ce3a3905e3989fe43888ba1
gpl <gpl@long-player.net>
2018-03-06 04:36:40 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ paquet-plan] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-plan_eo.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-plan?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plan_description' => 'Tiu kromprogramo plibonigas la privatspacan paĝon „Mapo de la retejo“ per malfaldebla strukturo kaj filtrilo pri statuso de artikoloj.',
+	'plan_nom' => 'Mapo de la retejo en la privata spaco',
+	'plan_slogan' => 'Plibonigas la mapon de la retejo en la privata spaco'
+);
