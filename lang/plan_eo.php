49639df22763f02dbeb177e5ac230288267bb86c
gpl <gpl@long-player.net>
2018-03-06 04:36:39 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ plan] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/plan_eo.php
@@ -0,0 +1,37 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/plan?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_deplacement' => 'Ĉu vi certas, ke vi volas transloki tiun elementojn ?',
+
+	// D
+	'deplacement_en_echec' => 'Malsukceso de translokado.',
+	'deplacement_identifiant_absent' => 'Neniu ',
+	'deplacement_reussi' => 'Translokado sukcesa.',
+	'deplacements_en_echec' => '@nb@ translokadoj malsukcesaj.',
+	'deplacements_reussis' => '@nb@ translokadoj sukcesaj.',
+
+	// E
+	'erreur_aucun_identifiant' => 'Neniu objektidentigilo.',
+	'erreur_autorisation_insuffisante' => 'Nesufiĉa rajtigo.',
+	'erreur_deplacement' => 'Translokado ne plenumita.',
+	'erreur_deplacement_impossible' => 'Ne eblas transloki !',
+	'erreur_rubriques_parentes_incorrectes' => 'Eraraj patraj rubrikoj.',
+
+	// P
+	'plan_titre' => 'Mapo de la retejo en la privata spaco',
+
+	// S
+	'suggerer_deplier' => 'Tio povas deveni de la translokado de granda grupo : tiuokaze, la listo de ĝiaj idoj ne enmemoriĝis. Vi devas unue malfaldi la grupon (kiu do enmemorigos la idojn), refaldi ĝin, kaj retransloki ĝin.',
+
+	// T
+	'tous_les_statuts' => 'Ĉiuj statusoj'
+);
