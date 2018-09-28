a001ed2c60c3de62d5d729ef611950bfc2d04fff
origenius <origenius@mvb.biglobe.ne.jp>
2018-09-28 03:40:14 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ paquet-plan] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/paquet-plan_ja.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-plan?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// P
+	'plan_description' => 'このプラグインは、折りたたみ可能な構造を表示し、記事のステータスでソートすることによって、管理エリアのページ「サイトマップ」を改善します。',
+	'plan_nom' => '管理エリアのサイトマップ',
+	'plan_slogan' => '管理エリアのサイトマップを改善する。'
+);
