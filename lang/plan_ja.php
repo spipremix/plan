28b520e3fa6d6f733f65f9334167ae981592a654
origenius <origenius@mvb.biglobe.ne.jp>
2018-09-28 03:40:12 +0000

    [Salvatore] [source:_core_/plugins/plan/lang/ plan] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/plan_ja.php
@@ -0,0 +1,38 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/plan?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'confirmer_deplacement' => 'これらのアイテムを移動してもよろしいですか?',
+
+	// D
+	'deplacement_en_echec' => '移動に失敗しました。',
+	'deplacement_identifiant_absent' => '無し',
+	'deplacement_reussi' => '成功した移動',
+	'deplacements_en_echec' => '失敗した移動@nb@件',
+	'deplacements_reussis' => '成功した移動@nb@件',
+
+	// E
+	'erreur_aucun_identifiant' => 'オブジェクトＩＤ無し',
+	'erreur_autorisation_insuffisante' => '承認が不十分です。',
+	'erreur_deplacement' => '移動は行われません。',
+	'erreur_deplacement_impossible' => '移動不可能！',
+	'erreur_rubriques_parentes_incorrectes' => '不適切な親セクション',
+
+	// P
+	'plan_titre' => '管理エリアのサイトマップ',
+
+	// S
+	'suggerer_deplier' => 'これは、大きなグループの移動によって引き起こされる可能性があります。
+その場合、子リストをロードできませんでした。ブロックを展開するとロードされます。それをもう一度折り畳んで、もう一度移動してください。',
+
+	// T
+	'tous_les_statuts' => '全ステータス'
+);
